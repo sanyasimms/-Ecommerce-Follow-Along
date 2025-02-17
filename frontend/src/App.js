e1891b0ee6e9bc37b2b3d45b19f4b2bebcd012a7
sanyasimms <jain.sanya0210@gmail.com>
2025-02-17 09:06:34 +0530

    Milestone-8 done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,21 @@
-import React from "react";
-import "./App.css"
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {LoginPage,SignUpPage} from "./Routess.js"// Import fixed
+import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import {LoginPage,  Home, ActivationPage, SignUpPage,CreateProduct} from "./Routess.js";
+import "./App.css";
 
 const App = () => {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/sign-up" element={<SignUpPage />} />
+    <Route path='/' element={<Home/>}/>
+      <Route path='/login' element={<LoginPage/>}/>
+      <Route path="/sign-up" element={<SignUpPage/>} />
+      <Route path="/activation/:activation_token" element={<ActivationPage />} />
+      <Route path='/create-product' element={<CreateProduct/>}/>
     </Routes>
-  </BrowserRouter>
-);
-};
+    </BrowserRouter>
+    
+  )
+}
 
-export default App;
+export default App
