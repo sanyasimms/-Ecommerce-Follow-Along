62a35c2407cb875722c63ce72c41ec5ff1db7b35
sanyasimms <jain.sanya0210@gmail.com>
2025-02-17 09:18:56 +0530

    Milestone-10 done

--- frontend/src/pages/ActivationPage.jsx
+++ frontend/src/pages/ActivationPage.jsx
@@ -3,7 +3,8 @@ import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-import { server } from '../server'
+// import { server } from '../server'
+const server = "http://localhost:8000";
 
 
 const ActivationPage = () => {
