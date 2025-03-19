98cd7b5fa85ab7be7b4c0e770957c659443dda5c
sanyasimms <jain.sanya0210@gmail.com>
2025-03-19 10:48:17 +0530

    done

--- frontend/src/Routess.js
+++ frontend/src/Routess.js
@@ -7,6 +7,7 @@ import MyProducts from "../src/pages/myProducts.jsx";
 import Cart from "./pages/cart.jsx";
 import ProductDetails from "./pages/productDetails.jsx";
 import Profile from "./pages/profile.jsx";
+import CreateAddress from "./pages/createAddress.jsx";
 export
 {
     LoginPage,
@@ -17,6 +18,7 @@ export
     MyProducts,
     Cart,
     ProductDetails,
-    Profile
+    Profile,
+    CreateAddress
 
 }
